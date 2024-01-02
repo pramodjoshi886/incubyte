a87b49c572d5df16f35b36a6b65f683b3f48a74d
pj1mb <joshipramod886@gmail.com>
2024-01-02 20:32:55 +0530

    m002: condition check and fix

--- String Calculator/src/main/StringCalculator.java
+++ String Calculator/src/main/StringCalculator.java
@@ -2,7 +2,10 @@ package main;
 
 public class StringCalculator {
 
-    public static int add(String numbers){
-        return 0;
+    public static int add(String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+        return -1;
     }
 }
