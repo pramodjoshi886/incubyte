a0552d90258bbcc2abce1eb884057bfeea0eedd8
pj1mb <joshipramod886@gmail.com>
2024-01-02 20:44:38 +0530

    m006: handling newLineBetweenNumbers

--- String Calculator/src/main/StringCalculator.java
+++ String Calculator/src/main/StringCalculator.java
@@ -11,7 +11,7 @@ public class StringCalculator {
         if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
-        String[] numberArray = numbers.split(",");
+        String[] numberArray = numbers.split("[,\n]");
         int sum = 0;
         for (String number : numberArray) {
             sum += Integer.parseInt(number);
