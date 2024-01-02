4e87182c9454f6fc368bf116e8846859adb7a9cb
pj1mb <joshipramod886@gmail.com>
2024-01-02 20:45:38 +0530

    m007: test custom delimiters

--- String Calculator/src/test/StringCalculatorTest.java
+++ String Calculator/src/test/StringCalculatorTest.java
@@ -27,4 +27,9 @@ public class StringCalculatorTest {
         assertEquals(6, StringCalculator.add("1\n2,3"));
     }
 
+    @Test
+    public void add_CustomDelimiter_ReturnsSum() {
+        assertEquals(3, StringCalculator.add("//;\n1;2"));
+    }
+
 }
