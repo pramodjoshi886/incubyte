3788124896b9a5e4d49905286c5a075d52b57f2c
pj1mb <joshipramod886@gmail.com>
2024-01-02 20:42:10 +0530

    m005: test newLineBetweenNumbers

--- String Calculator/src/test/StringCalculatorTest.java
+++ String Calculator/src/test/StringCalculatorTest.java
@@ -12,10 +12,19 @@ public class StringCalculatorTest {
         assertEquals(0, StringCalculator.add(""));
     }
 
-
+    @Test
+    public void add_SingleNumber_ReturnsNumber() {
+        assertEquals(1, StringCalculator.add("1"));
+    }
 
     @Test
     public void add_TwoNumbers_ReturnsSum() {
         assertEquals(3, StringCalculator.add("1,2"));
     }
+
+    @Test
+    public void add_NewLineBetweenNumbers_ReturnsSum() {
+        assertEquals(6, StringCalculator.add("1\n2,3"));
+    }
+
 }
